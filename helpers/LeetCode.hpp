2f12084d0ec1ce038334d42ba4f676d11c8d08db
BrainStone <yannick@tedworld.de>
2024-01-08 08:20:49 +0100

    Add first iteration of the helper header file

---
+++ helpers/LeetCode.hpp
@@ -0,0 +1,12 @@
+#ifndef LEETCODE_LEETCODE_HPP
+#define LEETCODE_LEETCODE_HPP
+
+// I know both of these are terrible practice, but it's what is pre-included by LeetCode.
+// You can disable LOCALLY by defining `LEETCODE_DISABLE_AUTO_STD`
+#ifndef LEETCODE_DISABLE_AUTO_STD
+#include <bits/stdc++.h>
+
+using namespace std;
+#endif
+
+#endif
